01f169aa141b0c9fee34dcb3b93664273501e612
jasonhcx <lucasg.ataide@gmail.com>
2020-04-12 17:20:07 +0000

    meajudepliz

--- tcp_client.c
+++ tcp_client.c
@@ -8,15 +8,16 @@ int main(int argc, char** argv) {
 
 	int client_fd; // file descriptor do socket do cliente
 
-	int port = argv[2];´
+	int port = argv[2];
 	
-	int client_name[100];
+	int client_name[40];
     
     strcpy(client_name, argv[3]);
 
 	char *host = argv[1];
 
-	char send_msg[100]; // string com mensagem a enviar
+	char msg[100]; // msg digitada pelo cliente
+	char send_msg[140]; // msg do cliente junto com o nome para envio
 	char recv_msg[100]; // string com mensagem a receber
 
 	struct sockaddr_in server_addr; // struct com informacoes do servidor a conectar
@@ -29,7 +30,7 @@ int main(int argc, char** argv) {
 	server_addr.sin_port = htons(port); // associa porta do servidor a struct
 	inet_aton(host, &server_addr.sin_addr); // associa endereco do servidor (armazenado na string host) a struct
 
-	printf("Conectando-se a porta %d do servidor %s\n", port, host);
+	printf("Conectando-se ao servidor %s na porta %d\n", host, port);
 
 	connect(client_fd, (struct sockaddr*) &server_addr, sizeof(server_addr)); // funcao bloqueante, conecta ao servidor
 
@@ -37,10 +38,12 @@ int main(int argc, char** argv) {
 		bzero( send_msg, 100);
 		bzero( recv_msg, 100);
 		
-		printf("Digite algo para enviar ao servidor: ");
-		fgets(send_msg, 100, stdin); //le do usuario string para enviar ao servidor
-
+		printf("Digite sua mensagem: ");
+		fgets(msg, 100, stdin); //le do usuario string para enviar ao servidor
+        strcat(send_msg, client_name);
+        strcat(send_msg, msg);
 		write(client_fd, send_msg, strlen(send_msg)+1);
+        
 		read(client_fd, recv_msg, 100); // le do servidor string para exibir para o usuario
 
 		printf("%s\n", recv_msg); // exibe o recebido na tela
